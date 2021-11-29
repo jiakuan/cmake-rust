1c192c864f0ffafa03b2231d71762a3efd9b4381
Jake Wang <delight.wjk@gmail.com>
2021-11-29 18:15:51 +0800

    Reverted to msvc with comments

--- cmake/CMakeCargo.cmake
+++ cmake/CMakeCargo.cmake
@@ -11,12 +11,16 @@ function(cargo_build)
             # dynamic debug for cpp code. No clean solution have been found to
             # this issue. Using the -gnu target on Windows for the Rust lib and
             # building with mingw bypasses this.
+            #
             # https://github.com/trondhe/rusty_cmake
-            # set(LIB_TARGET "x86_64-pc-windows-msvc")
-            set(LIB_TARGET "x86_64-pc-windows-gnu")
+            #
+            # But it seems the whole app has to be built with MinGW, so we
+            # are using MSVC with release build only at the moment
+            set(LIB_TARGET "x86_64-pc-windows-msvc")
+            # set(LIB_TARGET "x86_64-pc-windows-gnu")
         else()
-            # set(LIB_TARGET "i686-pc-windows-msvc")
-            set(LIB_TARGET "i686-pc-windows-gnu")
+            set(LIB_TARGET "i686-pc-windows-msvc")
+            # set(LIB_TARGET "i686-pc-windows-gnu")
         endif()
     elseif(ANDROID)
         if(ANDROID_SYSROOT_ABI STREQUAL "x86")
